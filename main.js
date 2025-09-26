7f7961ceeb4c29d946ac33d20b857ff6f40eb166
bran259 <kiprop686@gmail.com>
2025-09-26 11:24:17 +0300

    added

--- main.js
+++ main.js
@@ -5,6 +5,53 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const modal = document.getElementById('modal');
+modal.classList.add('hidden');
+
+
+const hearts = document.querySelectorAll('.like-glyph');
+
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {
+        
+        modal.classList.remove('hidden');
+        modal.innerText = error;
+
+    
+        setTimeout(() => {
+          modal.classList.add('hidden');
+        }, 3000);
+      });
+  });
+});
+
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < 0.2;
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
+
 
 
 //------------------------------------------------------------------------------
